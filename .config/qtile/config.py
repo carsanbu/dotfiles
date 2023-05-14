e33273b00ae15cb67e54f065e9f78886d66135f4
Carlos Sanmartín Bustos <carsanbu@gmail.com>
2023-05-14 12:45:54 +0200

    feat(rofi): bluetooth selector

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -111,7 +111,8 @@ if __name__ in ["config", "__main__"]:
             '~/.local/bin/launcher-poweroff.sh'))),
         Key([mod], 'l', lazy.spawn('multilockscreen --lock blur'), desc='Bloquea el escritorio'),
         Key([mod], 'v', lazy.spawn(os.path.expanduser('~/.local/bin/launcher-clipboard.sh')), desc='Historial del clipboard'),
-        Key([mod, SHIFT], 'e', lazy.spawn(os.path.expanduser('~/.local/bin/launcher-emoji.sh')), desc='Selector de emoji')
+        Key([mod, SHIFT], 'e', lazy.spawn(os.path.expanduser('~/.local/bin/launcher-emoji.sh')), desc='Selector de emoji'),
+        Key([mod, SHIFT], 'b', lazy.spawn(os.path.expanduser('~/.local/bin/launcher-bluetooth.sh')), desc='Selector de emoji')
     ]
 
     groups = [Group(name, **kwargs) for name, kwargs in group_names]
