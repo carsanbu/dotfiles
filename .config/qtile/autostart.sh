09d6e43fa1f524d6065c2b941fe5c77a871fb8f7
Carlos Sanmartín Bustos <carsanbu@gmail.com>
2024-01-03 23:24:10 +0100

    fix(qtile): autostart OpenRGB

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -15,6 +15,7 @@ run syncthing-gtk
 run greenclip daemon	# Clipboard history for rofi
 run xss-lock -l -- multilockscreen --lock blur	# Locker
 run flashfocus				# Animation on focus
+flatpak run org.openrgb.OpenRGB --server &
 
 # /etc/xdg/autostart mientras no pueda usar https://github.com/jceb/dex 
 run /usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1
@@ -22,7 +23,6 @@ run /usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1
 # Notificaciones
 #run $HOME/.local/bin/deadd-notification-center
 
-#flameshot &
 #run eww daemon
 #run eww open main_window
 
