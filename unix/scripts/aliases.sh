f9b65360827ed9c8a117c87910d0db53f9f96eed
Carlos Sanmartin <carsanbu@gmail.com>
2024-09-24 17:52:22 +0200

    feat(terminal): add aliases.sh
    fix terminal.conf.yaml using correct paths

---
+++ unix/scripts/aliases.sh
@@ -0,0 +1,10 @@
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+alias gs='git status'
+alias gd='git diff'
+alias gl='git log'
+
+alias cdw='cd $WORKSPACE && cd $(ls | fzf)'
