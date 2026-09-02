c38f4ef1f55daf402f89aa8d425b58d3cc583b6a
Carlos Sanmartín Bustos <carsanbu@gmail.com>
2026-09-02 13:19:10 +0200

    feat(nvim): save fzf history

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -8,3 +8,11 @@ require("mason-lspconfig").setup_handlers {
 require('lint').linters_by_ft = {
   markdown = {'markdownlint-cli2'},
 }
+
+-- Guardar historial para FZF (Ctrl+P, Ctrl+N para avanzar/retroceder)
+vim.g.fzf_history_dir = '~/.local/share/fzf-history'
+-- Búsqueda libre: te deja escribir el término
+vim.keymap.set('n', '<leader>a', ':Ag<CR>', { desc = 'Buscar en proyecto' })
+
+-- Búsqueda con la palabra bajo el cursor, lista para editar antes de confirmar
+vim.keymap.set('n', '<leader>aw', ':Ag <C-r><C-w><CR>', { desc = 'Buscar palabra bajo cursor' })
